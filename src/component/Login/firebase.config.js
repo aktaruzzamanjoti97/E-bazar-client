e3d82c478e6715cf3d2f91be83eff164d5f57900
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-04-07 21:18:43 +0600

    first commit

---
+++ src/component/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB99r0wGVrV4sM7DLK6CUc9lt2tACTlWus",
+  authDomain: "fullstack-assignment-6ec7f.firebaseapp.com",
+  projectId: "fullstack-assignment-6ec7f",
+  storageBucket: "fullstack-assignment-6ec7f.appspot.com",
+  messagingSenderId: "750656734037",
+  appId: "1:750656734037:web:7b849c665448221f1065dc"
+};
+
+export default firebaseConfig;
